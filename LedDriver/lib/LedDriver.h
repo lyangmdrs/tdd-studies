1a57534a9e75e6695579d9b9c75b3d4ea86a69a7
Lyang Medeiros <lyang.medeiros@ee.ufcg.edu.br>
2021-06-19 18:29:38 -0300

    feat: makes the test_TurnAllon() passes

--- LedDriver/lib/LedDriver.h
+++ LedDriver/lib/LedDriver.h
@@ -6,4 +6,6 @@
 void LedDriver_Create(uint16_t * address);
 void LedDriver_TurnOn(int ledNumber);
 void LedDriver_TurnOff(int ledNumber);
+void LedDriver_TurnAllOn(void);
+
 #endif
--- LedDriver/src/LedDriver.c
+++ LedDriver/src/LedDriver.c
@@ -16,3 +16,8 @@ void LedDriver_TurnOff(int ledNumber)
 {
     *ledsAddress = 0;
 }
+
+void LedDriver_TurnAllOn(void)
+{
+    *ledsAddress = 0xFFFF;
+}
